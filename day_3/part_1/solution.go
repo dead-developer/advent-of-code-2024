942995d1869f06403c8841fdc6bac39419e1c68c
Jukka Niemi <jukka.niemi73@hotmail.fi>
2024-12-03 15:50:20 +0200

    day 3

---
+++ day_3/part_1/solution.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"AoC2024/framework"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var line string
+
+func main() {
+	total := solution()
+	fmt.Println("solution:", total)
+}
+
+func solution() int {
+	loadData()
+
+	total := 0
+
+	re, _ := regexp.Compile(`mul\((\d+),(\d+)\)`)
+
+	for _, match := range re.FindAllStringSubmatch(line, -1) {
+		if len(match) == 3 {
+			num1, _ := strconv.Atoi(match[1])
+			num2, _ := strconv.Atoi(match[2])
+
+			total += num1 * num2
+		}
+	}
+
+	return total
+}
+
+func loadData() {
+	lines := framework.ReadInput("input.txt")
+
+	line = strings.Join(lines, "")
+
+}
