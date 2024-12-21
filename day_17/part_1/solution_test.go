c4a53fa02f15daaa08b284d100484caeec0e393d
Dead Developer <jukka.niemi73@hotmail.fi>
2024-12-21 09:06:32 +0200

    change test runner to use interfaces

--- day_17/part_1/solution_test.go
+++ day_17/part_1/solution_test.go
@@ -8,5 +8,5 @@ import (
 const correctAnswer = "7,3,1,3,6,3,6,0,2"
 
 func TestSolution(t *testing.T) {
-	framework.RunTestString(correctAnswer, solution(), t)
+	framework.RunTest(correctAnswer, solution(), t)
 }
--- framework/test_helpers.go
+++ framework/test_helpers.go
@@ -32,14 +32,7 @@ func getDayAndPart() (day, part string) {
 	return
 }
 
-func RunTest(correctAnswer int, value int, t *testing.T) {
-	day, part := getDayAndPart()
-
-	if value != correctAnswer {
-		t.Fatalf(`Day %s Part %s invalid result. Expect %d got %d`, day, part, correctAnswer, value)
-	}
-}
-func RunTestString(correctAnswer string, value string, t *testing.T) {
+func RunTest(correctAnswer interface{}, value interface{}, t *testing.T) {
 	day, part := getDayAndPart()
 
 	if value != correctAnswer {
