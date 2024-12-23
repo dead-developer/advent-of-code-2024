4fe510f20c9bac3aa02548274d96db38c828d964
Dead Developer <jukka.niemi73@hotmail.fi>
2024-12-23 11:35:32 +0200

    begin new solution

--- day_16/part_1/solution.go
+++ day_16/part_1/solution.go
@@ -3,40 +3,48 @@ package main
 import (
 	"AoC2024/framework"
 	"fmt"
-	"sort"
 	"strings"
 )
 
 var maze [][]string
+var visited map[point]bool
 
-var found []int
+var startPoint, endPoint point
+
+var queue []queueItem
+
+type queueItem struct {
+	cell     point
+	sourceId point
+	cost     int
+}
 
 type point struct {
 	x, y int
 }
 
-type reindeerStruct struct {
-	position  point
-	cost      int
-	direction int
-	visited   map[point]bool
+type edge struct {
+	from point
+	to   point
+	cost int
 }
 
-var start = point{}
-var theEnd = point{}
+type node struct {
+	id       point
+	edges    map[point]*edge
+	sourceId point
+	label    string
+}
+
+var nodes = make(map[point]*node)
+
+var nilPoint = point{-1, -1}
 
 var directions = map[int]point{
 	0: {0, -1}, //N
 	1: {1, 0},  //E
 	2: {0, 1},  //S
 	3: {-1, 0}, //W
-}
-
-var queue []queueItem
-
-type queueItem struct {
-	reindeer      reindeerStruct
-	goToDirection int
 }
 
 func main() {
@@ -47,118 +55,122 @@ func main() {
 
 func solution() int {
 	loadData("input.txt")
+	buildGraph()
 
-	firstReindeer := reindeerStruct{start, 0, 1, make(map[point]bool)}
-	firstReindeer.visited[start] = true
-	queue = append(queue, queueItem{reindeer: firstReindeer, goToDirection: -1}) // any direction
-
-	for len(queue) > 0 {
-		currentItem := queue[0]
-		queue = queue[1:]
-
-		cost := processPath(&currentItem.reindeer, currentItem.goToDirection)
-
-		if cost > 0 {
-			found = append(found, cost)
-		}
-		fmt.Println("queue len: ", len(queue))
-	}
-	// sort found
-	sort.Ints(found)
-	total := found[0]
-
-	return total
-}
-
-func processPath(reindeer *reindeerStruct, forceDirection int) int {
-	// where can I move?
-
-	for true {
-		availableDirections := getDirections(reindeer.position, reindeer)
-		if forceDirection > -1 {
-			availableDirections = []int{forceDirection}
-			forceDirection = -1
-		}
-		reindeer.visited[reindeer.position] = true
-		if theEnd.x == reindeer.position.x && theEnd.y == reindeer.position.y {
-			return reindeer.cost
-		}
-		if len(availableDirections) == 0 { // dead end
-			return 0
-		}
-		firstDirection := availableDirections[0]
-		availableDirections = availableDirections[1:]
-
-		//spawn more reindeers for other paths
-		for _, followDirection := range availableDirections {
-			// copy reindeer
-			newVisited := make(map[point]bool)
-			for k, v := range reindeer.visited {
-				newVisited[k] = v
-			}
-
-			newReindeer := reindeerStruct{reindeer.position, reindeer.cost, reindeer.direction, newVisited}
-			queue = append(queue, queueItem{reindeer: newReindeer, goToDirection: followDirection})
-		}
-		MoveTo(reindeer, firstDirection)
-
-	}
 	return 0
 }
 
-func MoveTo(reindeer *reindeerStruct, newDirection int) {
+//
+//func turnReindeer(towards int, reindeer *reindeerStruct) {
+//	if towards == reindeer.direction {
+//		return
+//	}
+//	diff := (towards - reindeer.direction + 4) % 4
+//	switch diff {
+//	case 1:
+//		reindeer.direction = (reindeer.direction + 1) % 4
+//		reindeer.cost += 1000
+//		break
+//	case 2:
+//		reindeer.direction = (reindeer.direction + 3) % 4
+//		reindeer.direction = (reindeer.direction + 3) % 4
+//		reindeer.cost += 2000
+//		break
+//	case 3:
+//		reindeer.direction = (reindeer.direction + 3) % 4
+//		reindeer.cost += 1000
+//		break
+//	}
+//
+//}
 
-	// if not facing the right way, turn
-	turnReindeer(newDirection, reindeer)
-	// move
-	reindeer.position.x += directions[newDirection].x
-	reindeer.position.y += directions[newDirection].y
-	reindeer.cost += 1
+func getNeighbours(location point) []point {
+	var availableDirections []point
 
-}
-
-func turnReindeer(towards int, reindeer *reindeerStruct) {
-	if towards == reindeer.direction {
-		return
-	}
-	diff := (towards - reindeer.direction + 4) % 4
-	switch diff {
-	case 1:
-		reindeer.direction = (reindeer.direction + 1) % 4
-		reindeer.cost += 1000
-		break
-	case 2:
-		reindeer.direction = (reindeer.direction + 3) % 4
-		reindeer.direction = (reindeer.direction + 3) % 4
-		reindeer.cost += 2000
-		break
-	case 3:
-		reindeer.direction = (reindeer.direction + 3) % 4
-		reindeer.cost += 1000
-		break
-	}
-
-}
-
-func getDirections(location point, reindeer *reindeerStruct) []int {
-	var availableDirections []int
-
-	for direction, directionVector := range directions {
-		if maze[location.y+directionVector.y][location.x+directionVector.x] != "#" {
-			if isVisited(point{location.x + directionVector.x, location.y + directionVector.y}, reindeer) {
+	for _, directionVector := range directions {
+		location := point{location.x + directionVector.x, location.y + directionVector.y}
+		if maze[location.y][location.x] != "#" {
+			if visited[location] {
 				continue
 			}
-			availableDirections = append(availableDirections, direction)
+			availableDirections = append(availableDirections, location)
 		}
 	}
 	return availableDirections
 }
 
-func isVisited(location point, reindeer *reindeerStruct) bool {
-	if _, ok := reindeer.visited[location]; ok {
-		return true
+func buildGraph() {
+	// create starting node
+	visited = make(map[point]bool)
+	queue = make([]queueItem, 0)
+
+	//nodes[startPoint] = node{id: startPoint, edges: make(map[int]int), sourceId: nilPoint}
+	queue = append(queue, queueItem{cell: startPoint, sourceId: nilPoint})
+
+	for len(queue) > 0 {
+		currentQueueItem := queue[0]
+		queue = queue[1:]
+
+		visited[currentQueueItem.cell] = true
+		// add node
+		newNode := node{id: currentQueueItem.cell, edges: make(map[point]*edge), sourceId: currentQueueItem.sourceId}
+		if currentQueueItem.cell == endPoint {
+			newNode.label = "E"
+		}
+		nodes[currentQueueItem.cell] = &newNode
+
+		neighbours := getNeighbours(currentQueueItem.cell)
+		for _, neighbour := range neighbours {
+			// add edge to source node
+			if currentQueueItem.sourceId != nilPoint {
+				newEdge := edge{from: currentQueueItem.sourceId, to: neighbour, cost: currentQueueItem.cost}
+				nodes[currentQueueItem.cell].edges[neighbour] = &newEdge
+			}
+			queue = append(queue, queueItem{cell: neighbour, sourceId: currentQueueItem.cell, cost: 1})
+
+		}
+		// neighbours are empty, remove parent node
 	}
-	return false
+	optimizeGraph()
+	visualize()
+
+}
+
+func optimizeGraph() {
+	// remove deadEnds
+	for nodeId := range nodes {
+		checkDeadEnd(nodeId)
+	}
+}
+
+func checkDeadEnd(nodeId point) {
+	if nodeId == nilPoint {
+		return
+	}
+	if _, ok := nodes[nodeId]; ok {
+		currentNode := nodes[nodeId]
+		if currentNode.label == "E" {
+			return
+		}
+		if len(currentNode.edges) == 0 {
+			deleteNode(nodeId)
+
+			// if dead end found, check source node
+			checkDeadEnd(currentNode.sourceId)
+			return
+		}
+	}
+}
+
+func deleteNode(nodeId point) {
+	if _, ok := nodes[nodeId]; ok {
+		// remove node and source nodes edge
+		currentNode := nodes[nodeId]
+		if currentNode.sourceId != nilPoint {
+			delete(nodes[currentNode.sourceId].edges, nodeId)
+		}
+		delete(nodes, nodeId)
+	}
 }
 
 func loadData(filename string) {
@@ -173,12 +185,12 @@ func loadData(filename string) {
 	for y, line := range maze {
 		for x, char := range line {
 			if char == "S" {
-				start = point{x: x, y: y}
+				startPoint = point{x: x, y: y}
 				maze[y][x] = "."
 				continue
 			}
 			if char == "E" {
-				theEnd = point{x: x, y: y}
+				endPoint = point{x: x, y: y}
 				maze[y][x] = "."
 				continue
 			}
@@ -187,25 +199,20 @@ func loadData(filename string) {
 
 }
 
-func visualize(reindeer *reindeerStruct) {
-
+func visualize() {
 	for y := 0; y < len(maze); y++ {
 		for x := 0; x < len(maze[y]); x++ {
-			if reindeer.position.x == x && reindeer.position.y == y {
-				fmt.Print("@")
-				continue
-			}
-
-			if isVisited(point{x, y}, reindeer) {
-				fmt.Print("*")
-			} else {
-				fmt.Print(maze[y][x])
-			}
+			fmt.Print(maze[y][x])
 		}
 		fmt.Println()
 	}
 	fmt.Println()
-	fmt.Println("Score:", reindeer.cost)
+
+	// print individiual nores
+	for _, node := range nodes {
+		fmt.Println(node.id, node.edges)
+	}
+
 }
 
 func waitForKeyPress() {
